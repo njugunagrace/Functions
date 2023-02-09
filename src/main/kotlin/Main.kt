410b4bcc9fd05a3a4154ed6e9f7c54201233682e
Grace <njugunagraceewambui@gmail.com>
2023-02-09 16:24:36 +0300

    assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main(){
+greeting("Grace")
+myFunction(45, 6)
+add(46,98,67, 98)
+fact()
+}
+
+fun greeting(name: String) {
+    println("Hello" + " " + name)
+
+}
+
+fun myFunction(num1: Int,Num2:Int){
+println(num1%Num2)
+}
+
+fun add(num1:Int,num2:Int, num3:Int, num4:Int){
+    var sum = num1+num2+num3+num4
+    println(sum)
+
+
+}
+fun fact(){
+     var factaboutMe =  ("I Love Myself")
+    println("I Love Myself")
+}
+
+
